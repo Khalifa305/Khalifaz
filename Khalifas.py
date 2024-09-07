0eb458715fa93a6705f9934e6a827690b162f424
Khalifa305 <Khalifaalmarri305@gmail.com>
2024-09-07 18:57:53 +0400

    version1

---
+++ Khalifas.py
@@ -0,0 +1,63 @@
+
+"Sample: 1!"
+
+def Project():
+    num1 = float(input("Enter your first number here: "))
+    num2 = float(input("Enter your second number now: "))
+
+    Addition = (num1 + num2)
+    Subtraction = (num1 - num2)
+    Division = (num1 / num2)
+    Multiply = (num1 * num2)
+    Mods = (num1 // num2)
+
+    print("Adding your numbers would give you:",Addition)
+    print("Subtracting your numbers would give you:",Subtraction)
+    print("Dividing your numbers would give you:",Division)
+    print("Modding your numbers would give you:",Mods)
+    print("Multiplying your numbers would give you:",Multiply)
+    
+print(__doc__)
+Project()
+
+"Sample: 2!"
+
+def your_pet():
+    age = int(input("Enter your age here:"))
+    mon = (age * 12)
+    petz = input("Enter your pet's name:")
+    pie = 3.14
+
+    print("Your age in Months would be:",mon)
+    print("And your pet's name is:",petz)
+    print("Fun fact: the actual value of pi is:",pie)
+
+print(__doc__)
+your_pet()
+
+"Sample: 3!"
+"IMPORTANT: Given the values that we'll be applying the calculations on the numbers 20 and 5.."
+
+def variablez():
+    nums1 = 20
+    nums2 = 5
+
+    Literalz = "Khalifa is cool."
+    Additionz = (nums1 + nums2)
+    Subtractionz = (nums1 - nums2)
+    Divisionz = (nums1 / nums2)
+    Modz = (nums1 % nums2)
+    Multiplyz = (nums1 * nums2)
+    Mixz = (float(nums1 * nums2 - nums2 / nums1))
+
+    print("When we add:",Additionz)
+    print("When we subtract:",Subtractionz)
+    print("When we divide:",Divisionz)
+    print("When we Mod:",Modz)
+    print("When we multiply:",Multiplyz)
+    print("When the mixed of the two numbers would be:",Mixz)
+
+print(__doc__)
+variablez()
+  
+
